aeb0d4ef58cfb6ebe30aa4cb9e061e374623bccb
Daniel <danielelacer08@gmail.com>
2025-11-16 22:00:19 +0000

    .c + .h -> .h++

--- bitset.h
+++ bitset.h
@@ -1,6 +1,8 @@
 #ifndef BITSET_H_
 #define BITSET_H_
 
+#include "error.h"
+#include "mem.h"
 #include <stddef.h>
 
 typedef struct Bitset
@@ -9,11 +11,53 @@ typedef struct Bitset
   unsigned char *buf;
 } Bitset;
 
-Bitset *B_create (size_t nbits);
-void B_set (Bitset *b, size_t bit, int val);
-int B_get (Bitset *b, size_t bit);
-void B_free (Bitset *b);
+static inline Bitset *B_create (size_t nbits);
+static inline void B_set (Bitset *b, size_t bit, int val);
+static inline int B_get (Bitset *b, size_t bit);
+static inline void B_free (Bitset *b);
 
 #define UCHAR_BITS 8
+
+static inline Bitset *
+B_create (size_t nbits)
+{
+  size_t nbytes = (nbits + (UCHAR_BITS - 1)) / UCHAR_BITS;
+
+  Bitset *b = NEW (Bitset);
+  b->buf = (unsigned char *)CALLOC (nbytes, sizeof (unsigned char));
+  b->nbytes = nbytes;
+
+  return b;
+}
+
+static inline void
+B_set (Bitset *b, size_t bit, int val)
+{
+  size_t idx = bit / UCHAR_BITS;
+  size_t offset = bit % UCHAR_BITS;
+  assert (idx < b->nbytes);
+
+  if (val)
+    b->buf[idx] |= (1U << offset);
+  else
+    b->buf[idx] &= ~(1U << offset);
+}
+
+static inline int
+B_get (Bitset *b, size_t bit)
+{
+  size_t idx = bit / UCHAR_BITS;
+  size_t offset = bit % UCHAR_BITS;
+  assert (idx < b->nbytes);
+
+  return (b->buf[idx] >> offset) & 1U;
+}
+
+static inline void
+B_free (Bitset *b)
+{
+  FREE (b->buf);
+  FREE (b);
+}
 
 #endif // BITSET_H_
--- mem.h
+++ mem.h
@@ -2,13 +2,16 @@
 #define MEM_H_
 
 #include "error.h"
+#include <stdlib.h>
 
-extern const Except_T Mem_Failed;
+static const Except_T Mem_Failed = { "Memory allocation failed" };
 
-void *Mem_alloc (long nbytes, const char *file, int line);
-void *Mem_calloc (long count, long nbytes, const char *file, int line);
-void Mem_free (void **ptr, const char *file, int line);
-void *Mem_resize (void *ptr, long nbytes, const char *file, int line);
+static inline void *Mem_alloc (long nbytes, const char *file, int line);
+static inline void *Mem_calloc (long count, long nbytes, const char *file,
+                                int line);
+static inline void Mem_free (void **ptr, const char *file, int line);
+static inline void *Mem_resize (void *ptr, long nbytes, const char *file,
+                                int line);
 
 #define ALLOC(nbytes) Mem_alloc ((nbytes), __FILE__, __LINE__)
 
@@ -27,5 +30,67 @@ void *Mem_resize (void *ptr, long nbytes, const char *file, int line);
 #define INIT(p) ((p) = ALLOC (sizeof (*p)))
 
 #define INIT0(p) ((p) = CALLOC (1, sizeof (*p)))
+
+static inline void *
+Mem_alloc (long nbytes, const char *file, int line)
+{
+  void *ptr;
+
+  assert (nbytes > 0);
+  ptr = malloc (nbytes);
+  if (!ptr)
+    {
+      if (!file)
+        RAISE (Mem_Failed);
+      else
+        Except_raise (&Mem_Failed, file, line);
+    }
+
+  return ptr;
+}
+
+static inline void *
+Mem_calloc (long count, long nbytes, const char *file, int line)
+{
+  void *ptr;
+
+  assert (count > 0);
+  assert (nbytes > 0);
+  ptr = calloc (count, nbytes);
+  if (!ptr)
+    {
+      if (!file)
+        RAISE (Mem_Failed);
+      else
+        Except_raise (&Mem_Failed, file, line);
+    }
+
+  return ptr;
+}
+
+static inline void
+Mem_free (void **ptr, const char *file, int line)
+{
+  if (*ptr)
+    free (*ptr);
+  *ptr = NULL;
+}
+
+static inline void *
+Mem_resize (void *ptr, long nbytes, const char *file, int line)
+{
+  assert (ptr);
+  assert (nbytes > 0);
+  ptr = realloc (ptr, nbytes);
+  if (!ptr)
+    {
+      if (!file)
+        RAISE (Mem_Failed);
+      else
+        Except_raise (&Mem_Failed, file, line);
+    }
+
+  return ptr;
+}
 
 #endif // MEM_H_
